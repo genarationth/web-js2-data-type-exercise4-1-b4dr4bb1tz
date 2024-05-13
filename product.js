7336fb150450106e6fdd9fe419f4d0d8b769848c
Soravit Sakukumpat <dareal.u.ess@gmail.com>
2024-05-13 14:32:00 +0700

    first obb

---
+++ product.js
@@ -0,0 +1,15 @@
+const product = {
+    product_Id: 1001,
+    productName: 'tuna',
+    quantityInStock: true,
+    unitPrice: 150,
+    productDescription: {
+        productBrand: 'Select',
+        sizeOrQuantity: 'Big',
+        testOrScent: 'มะเขือเทศ',
+        color: 'red',
+        additionalDescription: 'อร่อย'
+    }
+}
+
+console.log(product.product_Id, product.productName, product.quantityInStock);
